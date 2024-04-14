79c52d971be89f63d9b7ec1e1b9d05c8355bc176
hm.kim <hm.kim@ohoolabs.com>
2024-04-14 21:35:50 +0900

    feat: helmet

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Application from './components/Application';
+import { HelmetProvider } from 'react-helmet-async';
 
 // Say something
 console.log('[ERWT] : Renderer execution started');
 
 // Application to Render
-const app = <Application />;
+const app =
+    <HelmetProvider>
+        <Application />
+    </HelmetProvider>;
 
 // Render application in DOM
 createRoot(document.getElementById('app')).render(app);
